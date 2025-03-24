6899de6fe3121272d843b36b35b4d5062884012d
GuillaumeRhodes <rhodes.guillaume46@gmail.com>
2025-03-24 14:28:17 +0100

    add product model

--- core/models.py
+++ core/models.py
@@ -8,3 +8,10 @@ class Client(models.Model):
 
     def __str__(self):
         return self.name
+
+class Produit(models.Model):
+    nom = models.CharField(max_length=100)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.nom
