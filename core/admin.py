83351ce475a8cb131ffff0b4bf9d73de76c9a8f2
GuillaumeRhodes <rhodes.guillaume46@gmail.com>
2025-03-25 09:50:41 +0100

    add product

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Client
+from .models import Produit
 
 # Register your models here.
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'registered_at')
+    
+@admin.register(Produit)
+class ProduitAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'description')
